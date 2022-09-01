25492d27ffe385e2b839a16611efdd5b1e8a9d6b
hhxl <1042199702@qq.com>
2022-09-01 22:13:41 +0800

    删除

---
+++ ImageSRNet/test/testLoss.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+
+m = nn.Sigmoid()
+
+loss = nn.BCELoss(size_average=False, reduce=False)
+input = torch.randn(3, requires_grad=True)
+target = torch.empty(3).random_(2)
+lossinput = m(input)
+output = loss(lossinput, target)
