24d2b30bb69faac34c0a83c9ac2d21403cab7f50
hhxl <1042199702@qq.com>
2022-09-15 13:30:16 +0800

    测试loss

--- ImageSRNet/test/testLoss.py
+++ ImageSRNet/test/testLoss.py
@@ -1,12 +1,9 @@
 import torch
-import torch.nn as nn
-
-m = nn.Sigmoid()
-
-loss = nn.BCELoss(size_average=False, reduce=False)
-input = torch.randn(3, requires_grad=True)
-target = torch.empty(3).random_(2)
-lossinput = m(input)
-output = loss(lossinput, target)
-input = input.detach().numpy()
-print(output)
+from torch.nn import MaxPool2d
+input = torch.rand([30,50], dtype=torch.float32)
+print(input.shape)
+input = torch.reshape(input, ( -1, 30, 50))
+print(input.shape)
+maxpool1 = MaxPool2d(kernel_size=5)
+output = maxpool1(input)
+print(output.shape)
