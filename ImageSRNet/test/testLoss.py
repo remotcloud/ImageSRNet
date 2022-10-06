8804291879679d8c61f254cccd5da914310f5fa0
hhxl <1042199702@qq.com>
2022-10-06 21:03:31 +0800

    delete remote directory directoryname

--- ImageSRNet/test/testLoss.py
+++
@@ -1,9 +0,0 @@
-import torch
-from torch.nn import MaxPool2d
-input = torch.rand([30,50], dtype=torch.float32)
-print(input.shape)
-input = torch.reshape(input, ( -1, 30, 50))
-print(input.shape)
-maxpool1 = MaxPool2d(kernel_size=5)
-output = maxpool1(input)
-print(output.shape)
