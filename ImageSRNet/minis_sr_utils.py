1af705c9e7d9e4d57c2d79f35d894167b24312b3
hhxl <1042199702@qq.com>
2022-11-07 22:22:30 +0800

    1.修改存储函数，将存储的个体和信息存储在按层名称区分的文件中 2.加入DataSetManager函数，主要是： （1）将手写数据集进行固定存储为pickle，保证每次使用的是相同的数据集， （2）对结果进行比较的相关函数，比较神经网络、srnn、（原始数据）的结果 （3）存储表达式结果为json格式

--- ImageSRNet/minis_sr_utils.py
+++ ImageSRNet/minis_sr_utils.py
@@ -11,6 +11,11 @@ from torch.utils.tensorboard import SummaryWriter
 import torchvision.datasets as dsets
 import torchvision.transforms as transforms
 from loss import *
+from matplotlib import pyplot as plt
+
+from Minist_imageCGP import ConvNet
+
+
 def get_data(batch_size):
     '''
     获取手写数识别的数据加载器，训练、验证、测试的loader
@@ -59,14 +64,14 @@ def get_data(batch_size):
     return train_loader, validation_loader, test_loader
 
 
-def evolutionNAddLamdaCommon(evlutionParam, input, target, file, run_num, item, Individual: type ):
+def evolutionNAddLamdaCommon(evlutionParam, input, target, indiv_dir, run_num, item, Individual: type):
     """
     n+lamda 通用进化算法
     :param Individual:
     :param evlutionParam:
     :param input:
     :param target:
-    :param file:
+    :param indiv_dir:
     :param run_num:
     :param item:
     :return:
@@ -89,14 +94,14 @@ def evolutionNAddLamdaCommon(evlutionParam, input, target, file, run_num, item, 
         idiv = population[i]
         try:
             # idiv.fitness = loss_function(input, target, idiv)
-            setattr(idiv,'fitness',loss_function(input, target, idiv))
+            setattr(idiv, 'fitness', loss_function(input, target, idiv))
         except Exception as e:
             print(e)
             setattr(idiv, 'fitness', loss_function(input, target, idiv))
     # 寻找当代最优个体，并且记录了该个体
     best_individual = min(population, key=lambda x: x.fitness)
 
-    with open(file, "a") as f:
+    with open(indiv_dir, "a") as f:
         f.write(str(0) + " " + str(best_individual.fitness) + "\n")
     # 下一代种群
     newPopulation = [i for i in range(0, populationSize)]
@@ -118,11 +123,8 @@ def evolutionNAddLamdaCommon(evlutionParam, input, target, file, run_num, item, 
             best_individual = min(population, key=lambda x: x.fitness)
 
             if gen == genNum or gen % 30 == 0:
-                with open(file, "a") as f:
-                    fitness = getattr(best_individual, 'fitness')
-                    f.write(str(gen) + " " + str(fitness) + "\n")
-                # print(str(fitness))
-                # print("gen=" + str(gen))
+                fitness = getattr(best_individual, 'fitness')
+                print("gen " + str(gen) + " fitness " + str(fitness))
         if gen < (genNum - 1):
             # 将最优个体添加至下一代种群
             newPopulation[0] = best_individual
@@ -130,7 +132,7 @@ def evolutionNAddLamdaCommon(evlutionParam, input, target, file, run_num, item, 
             for i in range(1, math.ceil(populationSize * lamda)):
                 mutated_icgp = best_individual.mutate(mutate_prob)
                 # 计算新个体的适应度函数值
-                #mutated_icgp.fitness = loss_function(input, target, mutated_icgp)
+                # mutated_icgp.fitness = loss_function(input, target, mutated_icgp)
                 setattr(mutated_icgp, 'fitness', loss_function(input, target, mutated_icgp))
                 # 在变异产生的新个体和父代个体中选择最优秀的个体保存下来
                 if mutated_icgp.fitness <= best_individual.fitness:
@@ -140,31 +142,32 @@ def evolutionNAddLamdaCommon(evlutionParam, input, target, file, run_num, item, 
             for j in range(math.ceil(populationSize * lamda), populationSize):
                 new_icgp = Individual(evlutionParam)
                 # 计算新个体的适应度函数值
-                new_icgp.fitness = loss_function(input, target, new_icgp)
+                # new_icgp.fitness = loss_function(input, target, new_icgp)
+                setattr(new_icgp, 'fitness', loss_function(input, target, new_icgp))
                 # 在变异产生的新个体和父代个体中选择最优秀的个体保存下来
                 newPopulation[j] = new_icgp
         if gen % 1000 == 0:
-            #异步存储数据
+            # 异步存储数据
             # bestIndividual.fitness = bestIndividual.fitness.detach()
             # bestIndividual_copy = copy.deepcopy(bestIndividual)
-            p = Process(target=parallel_save_result, args=(best_individual,gen,run_num,item,))
+            p = Process(target=parallel_save_result, args=(indiv_dir, best_individual, gen, run_num, item,))
             print('Child process will start.')
             p.start()
     writer.close()
     return best_individual
 
-def parallel_save_result(best_individual, gen,run_num,item):
+
+def parallel_save_result(indiv_dir, best_individual, gen, run_num, run_times):
     """Save the best individual and the corresponding information"""
 
-    indiv_dir = f'CGPIndiva/'
-    save_dir = os.path.join(indiv_dir,f'{run_num}_{item}_indiv')
+    save_dir = os.path.join(indiv_dir, f'r{run_num}_t{run_times}_indiv')
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
-    fn = f'CGPIndiva/{run_num}_{item}_indiv/bestIndiv.pkl'
-    with open(fn, 'wb') as f:  # open file with write-mode
+    best_indiv_file = save_dir + '/bestIndiv.pkl'
+    with open(best_indiv_file, 'wb') as f:  # open file with write-mode
         pickle.dump(best_individual, f)  # serialize and save objec
 
-    #保存日志信息，每个表达式
+    # 保存日志信息，每个表达式
     bestExpression = best_individual.get_expressions()
     express_dict = {}
     for i, exp in enumerate(bestExpression):
@@ -172,12 +175,137 @@ def parallel_save_result(best_individual, gen,run_num,item):
 
     log_info = {
         'run_num': str(run_num),
-        'run_item': str(item),
+        'run_item': str(run_times),
         'generation': str(gen),
         'fitness': str(best_individual.fitness),
         'Expression': express_dict
     }
-    log_dir = f'CGPIndiva/{run_num}_{item}_indiv/log.json'
-    with open(log_dir, 'w') as f:  # open file with write-mode
-        json.dump(log_info, f ,indent=4)  # serialize and save objec
+    log_file = save_dir + '/log.json'
+    with open(log_file, 'w') as f:  # open file with write-mode
+        json.dump(log_info, f, indent=4)  # serialize and save objec
     print(f'{gen} generation program save OK!')
+
+
+class DataSetManager(object):
+    def __init__(self, best_individual=None):
+        self.bestIndividual = best_individual
+        self.dataSet = self.__getDataPickle()
+
+    def __getDataPickle(self):
+        fileDir = f'data/mnistData.pkl'
+        with open(fileDir, 'rb') as f:
+            nnMnistData = pickle.load(f)
+        return nnMnistData
+
+    def __saveDataPickle(self, batch_size=640):
+        global nndata
+        train_dataset = dsets.MNIST(root='./data',  # 文件存放路径
+                                    train=True,  # 提取训练集
+                                    # 将图像转化为 Tensor，在加载數据时，就可以对图像做预处理
+                                    transform=transforms.ToTensor(),
+                                    download=True)  # 当找不到文件的时候，自动下載
+        # 加载测试数据集
+        test_dataset = dsets.MNIST(root='./data',
+                                   train=False,
+                                   transform=transforms.ToTensor())
+        # 训练数据集的加载器，自动将数据切分成批，顺序随机打乱
+        train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                                   batch_size=batch_size,
+                                                   shuffle=True)
+        model = ConvNet()
+        fname = 'ministCnn'
+        model.load_state_dict(torch.load(f"ModelSave/{fname}.pth"))
+
+        if torch.cuda.is_available():
+            model = model.cuda()
+
+        for batch_idx, (data, target) in enumerate(train_loader):  # 针对容器中的每一个批进行循环
+            if torch.cuda.is_available():
+                data = data.cuda()
+                nndata = model(data)
+                break
+            else:
+                nndata = model(data)
+                break
+        fn = f'data/mnistData.pkl'
+        with open(fn, 'wb') as f:
+            pickle.dump(nndata, f)
+        return nndata
+
+    def getResultImageComp(self, batch_num):
+        """
+
+        :param batch_num:
+        :return:
+        """
+        # batch_num = 1
+        layer2InputData = self.dataSet[0]  # layer1CnnInput,layer1CnnOut,layer2nnInput,layer2CnnOutput
+        # 求出神经网络的输出
+        layer2DataOneBatchForNN = layer2InputData[batch_num - 1:batch_num]  # 神经网络需要维度[1,4,14,14]
+        Layer2SrOutData = self.bestIndividual(layer2DataOneBatchForNN)
+
+        layer2_cnn_network_out_data_forImage = self.dataSet[3][batch_num - 1]
+        layer2_data_input_for_image = layer2InputData[batch_num - 1]  # [4,14,14]
+        # fig = plt.figure(figsize=(10, 7))
+        # fig.suptitle('input')
+        # for i in range(layer2DataForImage.shape[0]):
+        #     plt.subplot(2, math.ceil( layer2DataOneBatch.shape[1]/ 2), i + 1)
+        #     plt.imshow(layer2Data[
+        #     batch_num][i, ...].cpu().data.numpy())
+        rowNum = 2
+        colNum = math.ceil(layer2_data_input_for_image.shape[0] / rowNum)
+
+        fig, cols = plt.subplots(rowNum, colNum)
+
+        fig.suptitle("layer2Input")
+        if len(cols.shape) == 1:  # cols只是一个一维数组
+            for row in range(rowNum):
+                for col in range(colNum):
+                    cols[row].imshow(layer2_data_input_for_image[col, ...].cpu().data.numpy())
+        if len(cols.shape) == 2:  # cols是一个二维数组
+            for row in range(rowNum):
+                for col in range(colNum):
+                    cols[row][col].imshow(layer2_data_input_for_image[col, ...].cpu().data.numpy())
+
+        fig = plt.figure(figsize=(10, 7))
+        fig.suptitle('outOfSrNet')
+        for i in range(Layer2SrOutData.shape[1]):
+            plt.subplot(2, math.ceil(Layer2SrOutData.shape[1] / 2), i + 1)
+            plt.imshow(Layer2SrOutData[0][i, ...].cpu().data.numpy())
+
+        fig = plt.figure(figsize=(10, 7))
+        fig.suptitle('outOfNet')
+        for i in range(layer2_cnn_network_out_data_forImage.shape[0]):
+            plt.subplot(2, math.ceil(layer2_cnn_network_out_data_forImage.shape[0] / 2), i + 1)
+            plt.imshow(layer2_cnn_network_out_data_forImage[i, ...].cpu().data.numpy())
+        plt.show()
+
+    def getResultImageComp2(self, number):
+        """
+        画图，得到卷积层2预测输出和实际输出的关系
+        :param number: input的数量
+        :return:
+        """
+        fig = plt.figure(figsize=(10, 7))
+        fig.suptitle('input')
+        row = math.ceil(math.sqrt(number))
+        col = math.ceil(number / row)
+        for imageId in range(number):
+            for i in range(col):
+                plt.subplot(row, col, imageId + 1)
+                plt.imshow(self.dataSet[2][imageId].unsqueeze(0)[0][0, ...].cpu().data.numpy())
+
+        plt.show()
+
+    def getResultJson(self):
+        bestExpression = self.bestIndividual.get_expressions()
+        express_dict = {}
+        for i, exp in enumerate(bestExpression):
+            express_dict[f'exp({i})'] = str(exp)
+        log_dict = {
+            "Expression": express_dict,
+            "fitness": str(self.bestIndividual.fitness)
+        }
+        with open(f'../result/result.json', 'w') as f:
+            json.dump(log_dict, f, indent=4)
+        print("Save json ok!")
