816ba4dae5736818c3f51b4bd99181cf9ff74135
hhxl <1042199702@qq.com>
2022-11-08 17:50:55 +0800

    把datamanager调到上面的位置

--- ImageSRNet/minis_sr_utils.py
+++ ImageSRNet/minis_sr_utils.py
@@ -16,6 +16,160 @@ from matplotlib import pyplot as plt
 from Minist_imageCGP import ConvNet
 
 
+class DataSetManager(object):
+    def __init__(self, best_individual=None, update_raw_data=False, update_network_data=False):
+        if update_network_data:
+            self.network_dataSet = self.__save_network_data_to_pickle(120)
+        else:
+            self.network_dataSet = self.__getDataPickle('data/network_data.pkl')
+        if update_raw_data:
+            self.raw_mnist_data = self.__save_raw_data_to_pickle(120)
+        else:
+            self.raw_mnist_data = self.__getDataPickle('data/raw_mnist_data.pkl')
+
+        self.bestIndividual = best_individual
+
+    def __getDataPickle(self, data_dir):
+
+        with open(data_dir, 'rb') as f:
+            nnMnistData = pickle.load(f)
+        return nnMnistData
+
+    def __save_raw_data_to_pickle(self, batch_size=640):
+        """Save the network data to pickle file"""
+        train_dataset = dsets.MNIST(root='./data',  # 文件存放路径
+                                    train=True,  # 提取训练集
+                                    # 将图像转化为 Tensor，在加载數据时，就可以对图像做预处理
+                                    transform=transforms.ToTensor(),
+                                    download=True)  # 当找不到文件的时候，自动下載
+        # 加载测试数据集
+        test_dataset = dsets.MNIST(root='./data',
+                                   train=False,
+                                   transform=transforms.ToTensor())
+        # 训练数据集的加载器，自动将数据切分成批，顺序随机打乱
+        train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                                   batch_size=batch_size,
+                                                   shuffle=True)
+
+        for batch_idx, (data, target) in enumerate(train_loader):  # 针对容器中的每一个批进行循环
+            fn = f'data/raw_mnist_data.pkl'
+            with open(fn, 'wb') as f:
+                pickle.dump([data, target], f)
+            return data
+
+    def __save_network_data_to_pickle(self, batch_size=640):
+        """Save the network data to pickle file"""
+
+        global nndata
+        train_dataset = dsets.MNIST(root='./data',  # 文件存放路径
+                                    train=True,  # 提取训练集
+                                    # 将图像转化为 Tensor，在加载數据时，就可以对图像做预处理
+                                    transform=transforms.ToTensor(),
+                                    download=True)  # 当找不到文件的时候，自动下載
+        # 加载测试数据集
+        test_dataset = dsets.MNIST(root='./data',
+                                   train=False,
+                                   transform=transforms.ToTensor())
+        # 训练数据集的加载器，自动将数据切分成批，顺序随机打乱
+        train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                                   batch_size=batch_size,
+                                                   shuffle=True)
+        model = ConvNet()
+        fname = 'ministCnn'
+        model.load_state_dict(torch.load(f"ModelSave/{fname}.pth"))
+
+        if torch.cuda.is_available():
+            model = model.cuda()
+
+        for batch_idx, (data, target) in enumerate(train_loader):  # 针对容器中的每一个批进行循环
+            if torch.cuda.is_available():
+                data = data.cuda()
+                nndata = model(data)
+                break
+            else:
+                nndata = model(data)
+                break
+        fn = f'data/network_data.pkl'
+        with open(fn, 'wb') as f:
+            pickle.dump(nndata, f)
+        return nndata
+
+    def getResultImageComp(self, batch_num, hidden_num):
+        """
+
+        :param hidden_num: 第几层网络
+        :param batch_num:
+        :return:
+        """
+        # batch_num = 1
+
+        hidden_input_data = self.network_dataSet[hidden_num-1]  # layer1CnnInput,layer1CnnOut,layer2nnInput,layer2CnnOutput
+        # 求出神经网络的输出
+        hidden_data_one_batch = hidden_input_data[batch_num - 1:batch_num]  # 神经网络需要维度[1,4,14,14]
+        sr_output = self.bestIndividual(hidden_data_one_batch)[0]
+
+        hidden_network_output = self.network_dataSet[hidden_num][batch_num - 1]
+        hidden_data_input_for_image = hidden_data_one_batch[0]  # [4,14,14]
+
+        rowNum = 2
+        colNum = math.ceil(hidden_data_input_for_image.shape[0] / rowNum)
+
+        fig, cols = plt.subplots(rowNum, colNum)
+
+        fig.suptitle("layer2Input")
+        if len(cols.shape) == 1:  # cols只是一个一维数组
+            for row in range(rowNum):
+                for col in range(colNum):
+                    cols[row].imshow(hidden_data_input_for_image[col, ...].cpu().data.numpy())
+        if len(cols.shape) == 2:  # cols是一个二维数组
+            for row in range(rowNum):
+                for col in range(colNum):
+                    cols[row][col].imshow(hidden_data_input_for_image[col, ...].cpu().data.numpy())
+
+        fig = plt.figure(figsize=(10, 7))
+        fig.suptitle('outOfSrNet')
+        for i in range(sr_output.shape[0]):
+            plt.subplot(2, math.ceil(sr_output.shape[0] / 2), i + 1)
+            plt.imshow(sr_output[i, ...].cpu().data.numpy())
+
+        fig = plt.figure(figsize=(10, 7))
+        fig.suptitle('outOfNet')
+        for i in range(hidden_network_output.shape[0]):
+            plt.subplot(2, math.ceil(hidden_network_output.shape[0] / 2), i + 1)
+            plt.imshow(hidden_network_output[i, ...].cpu().data.numpy())
+        plt.show()
+
+    def getResultImageComp2(self, number):
+        """
+        画图，得到卷积层2预测输出和实际输出的关系
+        :param number: input的数量
+        :return:
+        """
+        fig = plt.figure(figsize=(10, 7))
+        fig.suptitle('input')
+        row = math.ceil(math.sqrt(number))
+        col = math.ceil(number / row)
+        for imageId in range(number):
+            for i in range(col):
+                plt.subplot(row, col, imageId + 1)
+                plt.imshow(self.network_dataSet[2][imageId].unsqueeze(0)[0][0, ...].cpu().data.numpy())
+
+        plt.show()
+
+    def getResultJson(self, hidden_name):
+        bestExpression = self.bestIndividual.get_expressions()
+        express_dict = {}
+        for i, exp in enumerate(bestExpression):
+            express_dict[f'exp({i})'] = str(exp)
+        log_dict = {
+            "Expression": express_dict,
+            "fitness": str(self.bestIndividual.fitness)
+        }
+        with open(f'CGPIndiva/{hidden_name}/result.json', 'w') as f:
+            json.dump(log_dict, f, indent=4)
+        print("Save json ok!")
+
+
 def get_data(batch_size):
     '''
     获取手写数识别的数据加载器，训练、验证、测试的loader
@@ -175,7 +329,7 @@ def parallel_save_result(indiv_dir, best_individual, gen, run_num, run_times):
 
     log_info = {
         'run_num': str(run_num),
-        'run_item': str(run_times),
+        'run_times': str(run_times),
         'generation': str(gen),
         'fitness': str(best_individual.fitness),
         'Expression': express_dict
@@ -185,127 +339,3 @@ def parallel_save_result(indiv_dir, best_individual, gen, run_num, run_times):
         json.dump(log_info, f, indent=4)  # serialize and save objec
     print(f'{gen} generation program save OK!')
 
-
-class DataSetManager(object):
-    def __init__(self, best_individual=None):
-        self.bestIndividual = best_individual
-        self.dataSet = self.__getDataPickle()
-
-    def __getDataPickle(self):
-        fileDir = f'data/mnistData.pkl'
-        with open(fileDir, 'rb') as f:
-            nnMnistData = pickle.load(f)
-        return nnMnistData
-
-    def __saveDataPickle(self, batch_size=640):
-        global nndata
-        train_dataset = dsets.MNIST(root='./data',  # 文件存放路径
-                                    train=True,  # 提取训练集
-                                    # 将图像转化为 Tensor，在加载數据时，就可以对图像做预处理
-                                    transform=transforms.ToTensor(),
-                                    download=True)  # 当找不到文件的时候，自动下載
-        # 加载测试数据集
-        test_dataset = dsets.MNIST(root='./data',
-                                   train=False,
-                                   transform=transforms.ToTensor())
-        # 训练数据集的加载器，自动将数据切分成批，顺序随机打乱
-        train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                                   batch_size=batch_size,
-                                                   shuffle=True)
-        model = ConvNet()
-        fname = 'ministCnn'
-        model.load_state_dict(torch.load(f"ModelSave/{fname}.pth"))
-
-        if torch.cuda.is_available():
-            model = model.cuda()
-
-        for batch_idx, (data, target) in enumerate(train_loader):  # 针对容器中的每一个批进行循环
-            if torch.cuda.is_available():
-                data = data.cuda()
-                nndata = model(data)
-                break
-            else:
-                nndata = model(data)
-                break
-        fn = f'data/mnistData.pkl'
-        with open(fn, 'wb') as f:
-            pickle.dump(nndata, f)
-        return nndata
-
-    def getResultImageComp(self, batch_num):
-        """
-
-        :param batch_num:
-        :return:
-        """
-        # batch_num = 1
-        layer2InputData = self.dataSet[0]  # layer1CnnInput,layer1CnnOut,layer2nnInput,layer2CnnOutput
-        # 求出神经网络的输出
-        layer2DataOneBatchForNN = layer2InputData[batch_num - 1:batch_num]  # 神经网络需要维度[1,4,14,14]
-        Layer2SrOutData = self.bestIndividual(layer2DataOneBatchForNN)
-
-        layer2_cnn_network_out_data_forImage = self.dataSet[3][batch_num - 1]
-        layer2_data_input_for_image = layer2InputData[batch_num - 1]  # [4,14,14]
-        # fig = plt.figure(figsize=(10, 7))
-        # fig.suptitle('input')
-        # for i in range(layer2DataForImage.shape[0]):
-        #     plt.subplot(2, math.ceil( layer2DataOneBatch.shape[1]/ 2), i + 1)
-        #     plt.imshow(layer2Data[
-        #     batch_num][i, ...].cpu().data.numpy())
-        rowNum = 2
-        colNum = math.ceil(layer2_data_input_for_image.shape[0] / rowNum)
-
-        fig, cols = plt.subplots(rowNum, colNum)
-
-        fig.suptitle("layer2Input")
-        if len(cols.shape) == 1:  # cols只是一个一维数组
-            for row in range(rowNum):
-                for col in range(colNum):
-                    cols[row].imshow(layer2_data_input_for_image[col, ...].cpu().data.numpy())
-        if len(cols.shape) == 2:  # cols是一个二维数组
-            for row in range(rowNum):
-                for col in range(colNum):
-                    cols[row][col].imshow(layer2_data_input_for_image[col, ...].cpu().data.numpy())
-
-        fig = plt.figure(figsize=(10, 7))
-        fig.suptitle('outOfSrNet')
-        for i in range(Layer2SrOutData.shape[1]):
-            plt.subplot(2, math.ceil(Layer2SrOutData.shape[1] / 2), i + 1)
-            plt.imshow(Layer2SrOutData[0][i, ...].cpu().data.numpy())
-
-        fig = plt.figure(figsize=(10, 7))
-        fig.suptitle('outOfNet')
-        for i in range(layer2_cnn_network_out_data_forImage.shape[0]):
-            plt.subplot(2, math.ceil(layer2_cnn_network_out_data_forImage.shape[0] / 2), i + 1)
-            plt.imshow(layer2_cnn_network_out_data_forImage[i, ...].cpu().data.numpy())
-        plt.show()
-
-    def getResultImageComp2(self, number):
-        """
-        画图，得到卷积层2预测输出和实际输出的关系
-        :param number: input的数量
-        :return:
-        """
-        fig = plt.figure(figsize=(10, 7))
-        fig.suptitle('input')
-        row = math.ceil(math.sqrt(number))
-        col = math.ceil(number / row)
-        for imageId in range(number):
-            for i in range(col):
-                plt.subplot(row, col, imageId + 1)
-                plt.imshow(self.dataSet[2][imageId].unsqueeze(0)[0][0, ...].cpu().data.numpy())
-
-        plt.show()
-
-    def getResultJson(self):
-        bestExpression = self.bestIndividual.get_expressions()
-        express_dict = {}
-        for i, exp in enumerate(bestExpression):
-            express_dict[f'exp({i})'] = str(exp)
-        log_dict = {
-            "Expression": express_dict,
-            "fitness": str(self.bestIndividual.fitness)
-        }
-        with open(f'../result/result.json', 'w') as f:
-            json.dump(log_dict, f, indent=4)
-        print("Save json ok!")
